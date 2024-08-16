44e5a19f5befeec680f67140664bb5ceaefddfd3
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-08-17 01:31:24 +0530

    Animation is added between the navigation

--- app/src/main/java/com/angad/navigation/GameOverFragment.kt
+++ app/src/main/java/com/angad/navigation/GameOverFragment.kt
@@ -15,7 +15,7 @@ class GameOverFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_game_over, container, false)
 
--- app/src/main/java/com/angad/navigation/MainActivity.kt
+++ app/src/main/java/com/angad/navigation/MainActivity.kt
@@ -3,8 +3,6 @@ package com.angad.navigation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
